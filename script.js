a5233d3f5fe4b3e0dc7e94adb390705313d41c61
Bernardo <bernardo.cisternas@seb.se>
2024-06-20 12:49:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,64 @@ function random(min, max) {
 (function() {
     emailjs.init('z02IUa0U3uCyraPVI'); // Replace 'YOUR_USER_ID' with your actual user ID
 })();
+const translations = {
+    en: {
+        "page-title": "Lara turns 2!",
+        title: "Lara turns 2!",
+        welcome: "Welcome to the birthday party!",
+        date: "July 20 at 3 PM.",
+        address: "Centralvägen 15 at the back!",
+        "name-placeholder": "Name",
+        yes: "Yes! We are coming!",
+        no: "No, we cannot make it!",
+        send: "Send"
+    },
+    es: {
+        "page-title": "¡Lara cumple 2!!!",
+        title: "¡Lara cumple 2!",
+        welcome: "¡Bienvenidos a la fiesta de cumpleaños!",
+        date: "20 de julio a las 3 PM.",
+        address: "Centralvägen 15 en la parte trasera!",
+        "name-placeholder": "Nombre",
+        yes: "¡Sí! ¡Vamos a venir!",
+        no: "No, no podemos asistir.",
+        send: "Enviar"
+    },
+    sv: {
+        "page-title": "Lara fyller 2!!!",
+        title: "Lara fyller 2!",
+        welcome: "Välkomna på födelsedagsfest!",
+        date: "Den 20 juli kl 15.",
+        address: "Centralvägen 15 på baksidan!",
+        "name-placeholder": "Namn",
+        yes: "Ja! Vi kommer!",
+        no: "Nej, vi kan tyvärr inte!",
+        send: "Skicka"
+    }
+};
+
+function setLanguage(language) {
+    const elements = document.querySelectorAll('[data-translate]');
+    elements.forEach(element => {
+        const key = element.getAttribute('data-translate');
+        if (translations[language] && translations[language][key]) {
+            element.innerText = translations[language][key];
+        } else {
+            console.error(`Translation key '${key}' not found for language '${language}'`);
+        }
+    });
+
+    const placeholders = document.querySelectorAll('[data-translate-placeholder]');
+    placeholders.forEach(element => {
+        const key = element.getAttribute('data-translate-placeholder');
+        if (translations[language] && translations[language][key]) {
+            element.placeholder = translations[language][key];
+        } else {
+            console.error(`Translation key '${key}' not found for language '${language}'`);
+        }
+    });
+}
+
 // Function to animate the images bouncing like balls
 function animateImages() {
     const images = document.querySelectorAll('.floating-image');
@@ -54,7 +112,9 @@ function sendRSVP() {
         alert('Please fill in all fields.');
     }
 }
-
+document.addEventListener('DOMContentLoaded', () => {
+    setLanguage('sv');
+});
 // Call animateImages function when the window has loaded
 window.onload = function() {
     animateImages();
